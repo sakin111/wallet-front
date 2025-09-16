1af2997050618c33e404618c2f245e90e124e907
sakin111 <maliksakin53@gmail.com>
2025-09-17 01:36:55 +0600

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router'
 import './App.css'
 import Common from './components/layout/Common'
 import { useEffect, useState } from 'react';
+import { SkeletonCard } from './pages/SkeletonCard';
 
 function App() {
 
@@ -15,14 +16,7 @@ function App() {
 
   if (isLoading) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
-        {/* Skeleton or spinner */}
-        <div className="animate-pulse space-y-4 w-80">
-          <div className="h-6 bg-gray-200 rounded"></div>
-          <div className="h-6 bg-gray-200 rounded"></div>
-          <div className="h-6 bg-gray-200 rounded"></div>
-        </div>
-      </div>
+ <SkeletonCard/>
     );
   }
   return (
