c4c04d6fd8334b800d09e0119f04c9ec8d6c2b5e
sakin111 <maliksakin53@gmail.com>
2025-09-17 01:40:39 +0600

    .

--- src/pages/SkeletonCard.tsx
+++ src/pages/SkeletonCard.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export function SkeletonCard() {
   return (
- <div className="grid grid-cols-4">
+ <div className="grid grid-cols-4 gap-5">
        <div className="flex flex-col space-y-3">
       <Skeleton className="h-[125px] w-[250px] rounded-xl" />
       <div className="space-y-2">
