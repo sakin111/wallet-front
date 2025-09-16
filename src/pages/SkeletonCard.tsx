4856ccd34872f1f22183983e27f0ea7469c8903b
sakin111 <maliksakin53@gmail.com>
2025-09-17 01:43:57 +0600

    final commit

--- src/pages/SkeletonCard.tsx
+++ src/pages/SkeletonCard.tsx
@@ -6,7 +6,7 @@ export function SkeletonCard() {
        <div className="flex flex-col space-y-3">
       <Skeleton className="h-[125px] w-[250px] rounded-xl" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[251px]" />
         <Skeleton className="h-4 w-[200px]" />
       </div>
     </div>
